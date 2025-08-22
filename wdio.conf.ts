edb607255a8cf1c4fc45501bf183f348d7300618
prasannabj07 <prasannakumarpri7@gmail.com>
2025-08-22 13:17:19 +0530

    no message

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -138,7 +138,12 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec',
+    reporters: [
+        'spec',
+        ['junit', {
+            outputDir: './reports',       // folder where XMLs will be generated
+            outputFileFormat: (options) => `results-${options.cid}.xml`
+        }],
         ['allure', {
             outputDir: 'allure-results',
             disableWebdriverStepsReporting: true,
@@ -154,30 +159,10 @@ export const config: WebdriverIO.Config = {
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
-        // <string[]> (file/dir) require files before executing features
-        require: ['./features/step-definitions/steps.ts'],
-        // <boolean> show full backtrace for errors
-        backtrace: false,
-        // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
-        requireModule: [],
-        // <boolean> invoke formatters without executing steps
-        dryRun: false,
-        // <boolean> abort the run on first failure
-        failFast: false,
-        // <string[]> Only execute the scenarios with name matching the expression (repeatable).
-        name: [],
-        // <boolean> hide step definition snippets for pending steps
-        snippets: true,
-        // <boolean> hide source uris
-        source: true,
-        // <boolean> fail if there are any undefined or pending steps
-        strict: false,
-        // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tagExpression: 'smoke',
-        // <number> timeout for step definitions
-        timeout: 60000,
-        // <boolean> Enable this config to treat undefined definitions as warnings.
-        ignoreUndefinedDefinitions: false
+        require: ['./test/step-definitions/*.ts'],
+        format: ['junit:reports/results.xml'], // must match the Jenkinsfile path
+        tagExpression: '@smoke',
+        timeout: 60000
     },
 
 
